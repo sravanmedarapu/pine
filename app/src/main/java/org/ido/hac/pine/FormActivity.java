37ae4eba20f38bf9ad0b26bd314d8f1dec88c50e
prakashg <prakashg@PRINHYLTPAP1092.local>
2016-04-14 22:15:15 +0530

    Create form

---
+++ app/src/main/java/org/ido/hac/pine/FormActivity.java
@@ -0,0 +1,15 @@
+package org.ido.hac.pine;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by prakashg on 4/14/16.
+ */
+public class FormActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.form_activity);
+    }
+}
